1ddeaa99f7467fb33642b2ce5cd144d2a1141559
israelr9897 <israelr9897@gmail.com>
2025-09-01 12:38:38 +0300

    small chenge

--- first-react/src/hooks/useEffect/Section5.tsx
+++ first-react/src/hooks/useEffect/Section5.tsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 
+type nameObj = {
+  firstName: string | undefined;
+  lastName: string | undefined;
+  fullName: string | undefined;
+};
+
 export default function Section5() {
-  const [firName, setFirNmae] = useState<string>();
-  const [lasName, setLasNmae] = useState<string>();
-  const [fullName, setFullNmae] = useState<string>(" ");
+  const Name = {
+    firstName: undefined as string | undefined,
+    lastName: undefined as string | undefined,
+    fullName: undefined as string | undefined,
+  };
+  const [name, setNmae] = useState<nameObj>(Name);
   useEffect(() => {
-    setFullNmae(`${firName} ${lasName}`)
-  }, [firName, lasName]);
+    setNmae({ ...name, fullName: `${name.firstName} ${name.lastName}` });
+  }, [name.firstName, name.lastName]);
   return (
     <div>
       <label htmlFor="firstName">First name: </label>
       <input
         id="firstName"
         type="text"
-        onChange={(e) => setFirNmae(e.target.value)}
+        onChange={(e) => setNmae({ ...name, firstName: e.target.value })}
       />
       <label htmlFor="lastName">Last name: </label>
       <input
         id="lastName"
         type="text"
-        onChange={(e) => setLasNmae(e.target.value)}
+        onChange={(e) => setNmae({ ...name, lastName: e.target.value })}
       />
-      <h3>Full name: {fullName}</h3>
+      <h3>Full name: {name.fullName}</h3>
     </div>
   );
 }
